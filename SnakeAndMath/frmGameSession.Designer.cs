403f724075efd9983dd8d63c8f9c0d2abd77f1e8
Adam Ashraf <adamashraf110141@gmail.com>
2026-03-08 23:39:17 +0800

    Replace gameboard form pcb to panel, gameboard now separated in another form, add features that changes the difficulty text in the label in gamesessionform based on the difficulty

--- SnakeAndMath/frmGameSession.Designer.cs
+++ SnakeAndMath/frmGameSession.Designer.cs
@@ -33,10 +33,9 @@
             this.label2 = new System.Windows.Forms.Label();
             this.lblPlayer = new System.Windows.Forms.Label();
             this.pcbGameTitle = new System.Windows.Forms.PictureBox();
-            this.pcbGameBoard = new System.Windows.Forms.PictureBox();
+            this.pnlGameBoard = new System.Windows.Forms.Panel();
             btnDice = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pcbGameTitle)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pcbGameBoard)).BeginInit();
             this.SuspendLayout();
             // 
             // lblLevel
@@ -46,9 +45,9 @@
             this.lblLevel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.lblLevel.Font = new System.Drawing.Font("Ravie", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblLevel.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.lblLevel.Location = new System.Drawing.Point(894, 158);
+            this.lblLevel.Location = new System.Drawing.Point(893, 158);
             this.lblLevel.Name = "lblLevel";
-            this.lblLevel.Size = new System.Drawing.Size(259, 40);
+            this.lblLevel.Size = new System.Drawing.Size(281, 40);
             this.lblLevel.TabIndex = 2;
             this.lblLevel.Text = "Easy";
             this.lblLevel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -105,45 +104,42 @@
             this.pcbGameTitle.TabIndex = 1;
             this.pcbGameTitle.TabStop = false;
             // 
-            // pcbGameBoard
+            // pnlGameBoard
             // 
-            this.pcbGameBoard.Image = global::SnakeAndMath.Properties.Resources.Snake_and_Ladder_No_Background1;
-            this.pcbGameBoard.Location = new System.Drawing.Point(2, 3);
-            this.pcbGameBoard.Name = "pcbGameBoard";
-            this.pcbGameBoard.Size = new System.Drawing.Size(830, 734);
-            this.pcbGameBoard.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pcbGameBoard.TabIndex = 0;
-            this.pcbGameBoard.TabStop = false;
+            this.pnlGameBoard.Location = new System.Drawing.Point(1, 3);
+            this.pnlGameBoard.Name = "pnlGameBoard";
+            this.pnlGameBoard.Size = new System.Drawing.Size(850, 816);
+            this.pnlGameBoard.TabIndex = 7;
             // 
             // frmGameSession
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(0)))), ((int)(((byte)(40)))));
-            this.ClientSize = new System.Drawing.Size(1205, 898);
+            this.ClientSize = new System.Drawing.Size(1205, 859);
+            this.Controls.Add(this.pnlGameBoard);
             this.Controls.Add(btnDice);
             this.Controls.Add(this.lblPlayer);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.lblLevel);
             this.Controls.Add(this.pcbGameTitle);
-            this.Controls.Add(this.pcbGameBoard);
             this.ForeColor = System.Drawing.Color.Transparent;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
             this.Name = "frmGameSession";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Snake and Math - {difficulty}";
+            this.Load += new System.EventHandler(this.frmGameSession_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pcbGameTitle)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pcbGameBoard)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.PictureBox pcbGameBoard;
         private System.Windows.Forms.PictureBox pcbGameTitle;
         private System.Windows.Forms.Label lblLevel;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label lblPlayer;
+        private System.Windows.Forms.Panel pnlGameBoard;
     }
 }
--- SnakeAndMath/frmGameSession.cs
+++ SnakeAndMath/frmGameSession.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,9 +13,15 @@ namespace SnakeAndMath
 {
     public partial class frmGameSession : Form
     {
+        public string Level { get; set; } // property to hold the level information
         public frmGameSession()
         {
             InitializeComponent();
+        }
+
+        public void ChangeLabelText(string message)
+        {
+            lblLevel.Text = message;
         }
 
         private void label2_Click(object sender, EventArgs e)
@@ -30,6 +37,19 @@ namespace SnakeAndMath
         private void btnDice_Click(object sender, EventArgs e)
         {
 
+        }
+
+        GameBoard gameBoard = new GameBoard();
+        private void frmGameSession_Load(object sender, EventArgs e)
+        {
+            pnlGameBoard.Controls.Clear();     // optional: remove previous form
+            gameBoard.TopLevel = false;         // make it behave like a control
+            gameBoard.Dock = DockStyle.Fill;    // fill the panel
+            pnlGameBoard.Controls.Add(gameBoard);     // add form to panel
+            gameBoard.Show();                   // display the form
+
+            //set label text to the level selected in the previous form
+            lblLevel.Text = Level;
         }
     }
 }
