2579df8b054266ac277527696c3a964efa10e3c3
muziiiq <phytonz1807@gmail.com>
2026-03-19 19:51:42 +0800

    timer-feature-muziiiq

---
+++ SnakeAndMath/timer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace SnakeAndMath
+{
+    internal class PlayerTimer
+    {
+        private Timer timer;
+        private int timeLeft;
+
+        public event Action TimeUp;
+        public event Action<int> TimeChanged;
+
+        public void Start(int seconds)
+        {
+            timeLeft = seconds;
+            timer = new Timer(1000);
+            timer.Elapsed += OnTimeTick;
+            timer.Start();
+        }
+
+        private void OnTimeTick(object sender, ElapsedEventArgs e)
+        {
+            timeLeft--;
+            TimeChanged?.Invoke(timeLeft);
+
+            if (timeLeft <= 0)
+            {
+                timer.Stop();
+                TimeUp?.Invoke();
+            }
+        }
+
+        public void Stop()
+        {
+            timer?.Stop();
+        }
+    }
+}
